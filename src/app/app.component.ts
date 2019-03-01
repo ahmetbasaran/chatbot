71686ede0352d1eb1e01924c5f4be1029e6e1390
kanderson1122 <k.anderson@nextech.com>
2019-03-01 08:04:48 -0500

    final commit 3 animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { NexbotService } from './services/nexbot.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('msgIn', [
+      state('void', style({
+        transform: 'scale(.01)'
+      })),
+      state('*', style({
+        transform: 'scale(1)'
+      })),
+      transition('void=>*', animate('100ms'))
+    ]),
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'nexbot';
